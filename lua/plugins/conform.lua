cb4ba4fdb9cf4b20c47a2abe29fc01f19a8c91f6
Daniel <maildanieln@gmail.com>
2026-05-18 19:31:51 -0600

    conform.nvim. If it works I'll remove null-ls entirely.

---
+++ lua/plugins/conform.lua
@@ -0,0 +1,15 @@
+-- conform.nvim for formatting
+
+return {
+    "stevearc/conform.nvim",
+    opts = {},
+    config = function ()
+        require('conform').setup({
+            formatters_by_ft = {
+                lua = { "stylua" },
+                c = { "clang-format" },
+                cpp = { "clang-format" },
+            },
+        })
+    end
+}

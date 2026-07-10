576013d0d6202e97fb33f7e743641cb026db61fb
Daniel Nzekwe <nzekwe.da@wcs-g.com>
2026-07-09 18:56:23 -0600

    Move stuff in the settings subtable out in zls.lua

--- lsp/zls.lua
+++ lsp/zls.lua
@@ -1,10 +1,8 @@
 return {
     server = 'zls',
     cmd = { 'zls' },
-    settings = {
-        filetypes = { 'zig', 'zir' },
-        root_markers = { 'build.zig' },
-        single_file_support = false,
-        workspace_required = false,
-    },
+    filetypes = { 'zig', 'zir' },
+    root_markers = { 'build.zig' },
+    single_file_support = false,
+    workspace_required = false,
 }

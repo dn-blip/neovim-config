623bf2960b3fc41290abb7a1ee3020bc9ec8a2b4
Daniel Nzekwe <nzekwe.da@wcs-g.com>
2026-07-09 18:56:51 -0600

    Update zls.lua

--- lsp/zls.lua
+++ lsp/zls.lua
@@ -2,7 +2,7 @@ return {
     server = 'zls',
     cmd = { 'zls' },
     filetypes = { 'zig', 'zir' },
-    root_markers = { 'build.zig' },
+    root_markers = { 'build.zig.zon', 'build.zig', '.git' },
     single_file_support = false,
     workspace_required = false,
 }

9cf0fd0b61cabab8cb690f455d3bd5ad313bce71
Daniel <maildanieln@gmail.com>
2026-07-15 00:44:45 -0600

    Push and pull!

--- ftplugin/cpp.lua
+++ ftplugin/cpp.lua
@@ -1,2 +1,7 @@
 -- Force 8 spaces for C++ files
-dofile('ftplugin/c.lua')
+vim.opt_local.tabstop = 8
+vim.opt_local.shiftwidth = 8
+vim.opt_local.softtabstop = 8
+vim.opt_local.expandtab = true -- Use spaces instead of tabs
+
+vim.treesitter.start()

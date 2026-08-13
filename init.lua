df1799d5773cc725c555edcb1f4b2fd5cc0bbf02
Daniel Nzekwe <nzekwe.da@wcs-g.com>
2026-08-13 13:50:33 -0600

    Starting to refactor init.lua
    Hopefully it doesn't break anything :)

--- init.lua
+++ init.lua
@@ -30,19 +30,89 @@ vim.pack.add({
     { src = cb('andyg/leap.nvim') },
 })
 
-for _, source in ipairs({
-    'mappings',
-    'plugin_mappings',
-    'options',
-    'autocmds',
-}) do
-    local ok, fault = pcall(require, source)
-    if not ok then vim.api.nvim_err_write('Failed to load ' .. source .. '\n\n' .. fault) end
-end
+----- options -----
+vim.g.mapleader = ' '
+local opt = vim.opt
 
-require('plugins').setup()
+opt.clipboard:append('unnamedplus')
+
+opt.encoding = 'utf-8'
+opt.matchpairs = { '(:)', '{:}', '[:]', '<:>' }
+
+local indent = 4
+opt.autoindent = true
+opt.expandtab = true
+opt.shiftwidth = indent
+opt.smartindent = true
+opt.tabstop = indent
+
+opt.number = true
+opt.relativenumber = true
+
+opt.cursorline = true
+opt.termguicolors = true
+opt.showmode = false
+opt.hlsearch = false
+
+opt.scrolloff = 8
+opt.wrap = true
+
+opt.history = 100 -- keep 100 lines of history
+opt.redrawtime = 1500
+opt.timeoutlen = 300 -- time to wait for a mapped sequence to complete (in milliseconds)
+opt.ttimeoutlen = 10
+opt.updatetime = 100
 
 vim.cmd([[filetype plugin indent on]])
+----- end of options -----
+
+----- key mappings -----
+local map = vim.keymap.set
+
+map('i', 'jj', '<ESC>')
+
+map('n', '<leader>w', '<cmd>w<CR>', { desc = 'save current buffer' })
+map('n', '<leader>q', '<cmd>q<CR>', { desc = '' })
+
+map('n', '<leader>h', '<C-w>h', { desc = 'switch window left' })
+map('n', '<leader>l', '<C-w>l', { desc = 'switch window right' })
+map('n', '<leader>k', '<C-w>k', { desc = 'switch window up' })
+map('n', '<leader>j', '<C-w>j', { desc = 'switch window down' })
+
+map({ 'n', 'i', 'v' }, '<Up>', '<nop>')
+map({ 'n', 'i', 'v' }, '<Down>', '<nop>')
+map({ 'n', 'i', 'v' }, '<Left>', '<nop>')
+map({ 'n', 'i', 'v' }, '<Right>', '<nop>')
+
+map(
+    'n',
+    '<leader>pb',
+    function() require('mini.pick').builtin.files() end,
+    { desc = '[p]ick [b]uffers (mini.nvim)' }
+)
+map(
+    'n',
+    '<leader>pf',
+    function() require('mini.pick').builtin.files() end,
+    { desc = '[p]ick [f]iles (mini.nvim)' }
+)
+map(
+    'n',
+    '<leader>ph',
+    function() require('mini.pick').builtin.help() end,
+    { desc = '[p]ick [h]elp menu entries (mini.nvim)' }
+)
+
+map('n', '<leader>?', function() require('which-key').show({ global = true }) end)
+map({ 'n', 'v' }, '<leader>f', '<cmd>Yazi<cr>', { desc = 'Open yazi at current file' })
+map({ 'n', 'v' }, '<leader>fr', '<cmd>Yazi toggle<cr>', { desc = 'Resume last yazi session' })
+map({ 'n', 'v' }, '<leader>fd', '<cmd>Yazi cwd', { desc = 'Open Yazi in current directory' })
+map({ 'n', 'x', 'o' }, 's', '<Plug>(leap)', { desc = 'leap search local' })
+map('n', 'S', '<Plug>(leap-from-window)', { desc = 'leap in other window' })
+----- end of key mappings -----
+
+require('autocmds')
+require('plugins').setup()
 vim.cmd('colorscheme sora')
 
 -- Enable LSPs,

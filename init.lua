e823d438076c264aaa6031a4963ddcc917c9d6da
Daniel Nzekwe <nzekwe.da@wcs-g.com>
2026-08-15 23:51:29 -0600

    Update init.lua

--- init.lua
+++ init.lua
@@ -16,8 +16,6 @@ vim.pack.add({
     { src = gh('mason-org/mason.nvim') },
     { src = gh('WhoIsSethDaniel/mason-tool-installer.nvim') },
     { src = gh('nvim-lua/plenary.nvim') },
-    { src = gh('DrKJeff16/wezterm-types') },
-    --{ src = gh('folke/lazydev.nvim') },
     { src = gh('folke/which-key.nvim') },
     { src = gh('antonk52/filepaths_ls.nvim') },
     { src = gh('mfussenegger/nvim-lint') },

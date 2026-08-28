f35e17ec94477ad4e14eef96c787a0533b5bf310
Daniel Nzekwe <nzekwe.da@wcs-g.com>
2026-08-28 11:21:40 -0600

    Update plugins.lua

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -96,7 +96,7 @@ local setup_codetools = function()
     require('conform').setup({
         formatters_by_ft = {
             lua = { 'stylua' },
-            c = { 'clang-format' },
+            --c = { 'clang-format' },
             cpp = { 'clang-format' },
             css = { 'csskit' },
             go = { 'gofmt' },
